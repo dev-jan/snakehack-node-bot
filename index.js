46c52e5e1260065d26c27ddfe00b4486ba0cd01b
Jan Bucher <jan@jan-bucher.ch>
2017-11-11 13:46:02 +0100

    Show js errors in console

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.use(function (err, req, res, next) {
 // 500 handler middleware, respond with JSON only
 app.use(function (err, req, res, next) {
   var statusCode = err.status || 500
+  console.error(err.stack);
 
   res.status(statusCode)
   res.send({
