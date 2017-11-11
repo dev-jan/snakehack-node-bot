3cbb6ecbcd6d8785f89351165d620543f906d01c
marco <marco.palkoska@stud.hslu.ch>
2017-11-11 12:24:51 +0100

    very important change

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.post('/start', function (req, res) {
   var data = {
     color: "#ff3030",
     name: "'; DROP DATABASE Snake",
-    head_url: "https://i.imgur.com/AOc1DjF.png",
+    head_url: "https://i.imgur.com/NRGrAxP.png",
     head_type: "tongue",
     tail_type: "small-rattle",
     taunt: "Something..."
