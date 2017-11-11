393e9c1ebafb01df6c4be3ea008968ce0e7e1acc
Jan Bucher <jan@jan-bucher.ch>
2017-11-11 17:01:38 +0100

    Implement near collision detection

--- snake-ai/Food.js
+++ snake-ai/Food.js
@@ -1,6 +1,8 @@
 var gameObjects = require('./GameObjects.js')
 
 module.exports = {
+    exitStategy: 'right',
+
     getNextMove: function(game) {
         var nearestFood = game.nearestFood
         var myPosition = game.myHeadPosition
@@ -28,29 +30,29 @@ module.exports = {
     fallbackIfNeeded: function (game, positionX, positionY, direction, recursionCounter) {
       console.log("try " + direction)
         if (recursionCounter > 20) {
-          console.log("recursion overflow :(")
-          return "up"
+          console.log("recursion overflow, use exit strategy " + this.exitStategy)
+          return this.exitStategy
         }
         if (direction === 'up') {
-          if (!this.isCollision(game, positionX, positionY - 1)) {
+          if (!this.isCollision(game, positionX, positionY - 1, direction)) {
               return 'up'
             }
             return this.fallbackIfNeeded(game, positionX, positionY, this.getFallbackDirection('up'), recursionCounter + 1)
         }
         if (direction === 'down') {
-          if (!this.isCollision(game, positionX, positionY + 1)) {
+          if (!this.isCollision(game, positionX, positionY + 1, direction)) {
             return 'down'
           }
           return this.fallbackIfNeeded(game, positionX, positionY, this.getFallbackDirection('down'), recursionCounter + 1)
         }
         if (direction === 'right') {
-          if (!this.isCollision(game, positionX + 1, positionY)) {
+          if (!this.isCollision(game, positionX + 1, positionY, direction)) {
             return 'right'
           }
           return this.fallbackIfNeeded(game, positionX, positionY, this.getFallbackDirection('right'), recursionCounter + 1)
         }
         if (direction === 'left') {
-          if (!this.isCollision(game, positionX - 1, positionY)) {
+          if (!this.isCollision(game, positionX - 1, positionY, direction)) {
             return 'left'
           }
           return this.fallbackIfNeeded(game, positionX, positionY, this.getFallbackDirection('left'), recursionCounter + 1)
@@ -72,19 +74,61 @@ module.exports = {
       }
     },
 
-    isCollision: function(game, x, y) {
+    isCollision: function(game, x, y, nextDirection) {
       if (x < 0 || y < 0) {
         return true
       }
       if (x >= game.gameWidth || y >= game.gameHeight) {
         return true
       }
-      if (game.gameArray[x][y] === gameObjects.Empty) {
+      if (!this.isFieldACollision(game, x, y)) {
+        console.log(nextDirection + " is no collision, check near collision...")
+        return this.nearCollition(game, x, y, nextDirection)
+      }
+      return true
+    },
+
+    isFieldACollision: function(game, x, y) {
+      // border is not a near collision...
+      if (x < 0 || y < 0) {
         return false
       }
-      if (game.gameArray[x][y] === gameObjects.Food) {
+      if (x >= game.gameWidth || y >= game.gameHeight) {
+        return false
+      }
+      if (game.gameArray[x][y] === gameObjects.Empty || game.gameArray[x][y] === gameObjects.Food) {
         return false
       }
       return true
+    },
+
+    nearCollition: function(game, x, y, direction) {
+      if (direction === 'up') {
+        if (this.isFieldACollision(game, x, y - 1)) {
+          console.log(direction + " is near collision!")
+          this.exitStategy = 'up'
+          return true
+        }
+      }
+      if (direction === 'right') {
+        if (this.isFieldACollision(game, x + 1, y)) {
+          this.exitStategy = 'right'
+          return true
+        }
+      }
+      if (direction === 'down') {
+        if (this.isFieldACollision(game, x, y + 1)) {
+          this.exitStategy = 'down'
+          return true
+        }
+      }
+      if (direction === 'left') {
+        if (this.isFieldACollision(game, x - 1, y)) {
+          this.exitStategy = 'left'
+          return true
+        }
+      }
+      console.log("no near collision :)")
+      return false
     }
 }
