d3479aade461a101d33b77b7e4d8b65e1b184f64
marco <marco.palkoska@stud.hslu.ch>
2017-11-11 13:22:59 +0100

    place all snake parts in game array

--- snake-ai/GameArrayBuilder.js
+++ snake-ai/GameArrayBuilder.js
@@ -4,19 +4,37 @@ module.exports = {
 
     createGameArray: function(data) {
 
+        console.log("GameBuilder.createGameArray()")
+
         var gameArray = createArray(data.width, data.height)
+
+        var myId = data.you
 
         // put snakes on game array
         data.snakes.forEach(function(snake) {
+
+            var isHead = true; // todo: make sure this is the head.. maybe it's the tail
             snake.coords.forEach(function(coord) {
-                game[coord[0], coord[1]] = gameObjects.EnemySnakeBody
-                console.log(game)
+
+                var objectToPlace = gameObjects.EnemySnakeBody
+                if (isHead) {
+                    if (snake.id == myId) {
+                        objectToPlace = gameObjects.PlayerSnakeHead
+                    } else {
+                        objectToPlace = gameObjects.EnemySnakeHead
+                    }
+                } else if (snake.id == myId) {
+                    objectToPlace = gameObjects.PlayerSankeBody
+                }
+                
+                gameArray[coord[0]][coord[1]] = objectToPlace
             }, this)
         }, this)
 
         return gameArray
     },
 
+    // Copy pasta from Stackoverflow. Creates 2 dimensional array
     createArray: function(length) {
         var arr = new Array(length || 0),
             i = length;
