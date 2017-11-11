fc05c0104cd4e13395da6d0407a3e6a372ce8e77
Jan Bucher <jan@jan-bucher.ch>
2017-11-11 15:20:18 +0100

    better fallback

--- snake-ai/Food.js
+++ snake-ai/Food.js
@@ -26,41 +26,57 @@ module.exports = {
     },
 
     fallbackIfNeeded: function (game, positionX, positionY, direction, recursionCounter) {
-        if (recursionCounter > 4) {
-          console.log("DEAD :(")
+      console.log("try " + direction)
+        if (recursionCounter > 20) {
+          console.log("recursion overflow :(")
           return "up"
         }
         if (direction === 'up') {
           if (!this.isCollision(game, positionX, positionY - 1)) {
               return 'up'
             }
-            return this.fallbackIfNeeded(game, positionX, positionY, 'right', recursionCounter + 1)
+            return this.fallbackIfNeeded(game, positionX, positionY, this.getFallbackDirection('up'), recursionCounter + 1)
         }
         if (direction === 'down') {
           if (!this.isCollision(game, positionX, positionY + 1)) {
             return 'down'
           }
-          return this.fallbackIfNeeded(game, positionX, positionY, 'left', recursionCounter + 1)
+          return this.fallbackIfNeeded(game, positionX, positionY, this.getFallbackDirection('down'), recursionCounter + 1)
         }
         if (direction === 'right') {
           if (!this.isCollision(game, positionX + 1, positionY)) {
             return 'right'
           }
-          return this.fallbackIfNeeded(game, positionX, positionY, 'up', recursionCounter + 1)
+          return this.fallbackIfNeeded(game, positionX, positionY, this.getFallbackDirection('right'), recursionCounter + 1)
         }
         if (direction === 'left') {
           if (!this.isCollision(game, positionX - 1, positionY)) {
             return 'left'
           }
-          return this.fallbackIfNeeded(game, positionX, positionY, 'down', recursionCounter + 1)
+          return this.fallbackIfNeeded(game, positionX, positionY, this.getFallbackDirection('left'), recursionCounter + 1)
         }
+    },
+
+    getFallbackDirection: function(direction) {
+      if (direction === 'up') {
+        return 'right'
+      }
+      if (direction === 'right') {
+        return 'down'
+      }
+      if (direction === 'down') {
+        return 'left'
+      }
+      if (direction === 'left') {
+        return 'up'
+      }
     },
 
     isCollision: function(game, x, y) {
       if (x < 0 || y < 0) {
         return true
       }
-      if (x > game.gameWidth || y > game.gameHeight) {
+      if (x >= game.gameWidth || y >= game.gameHeight) {
         return true
       }
       if (game.gameArray[x][y] === gameObjects.Empty) {
