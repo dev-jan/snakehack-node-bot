fa170c6a973a3d60a11c04c5be83f09e1ac9b8f0
marco <marco.palkoska@stud.hslu.ch>
2017-11-11 13:13:16 +0100

    added nearest food property to Game

--- snake-ai/Game.js
+++ snake-ai/Game.js
@@ -5,12 +5,17 @@ module.exports = {
     gameArray: null,
     myHeadPosition: null,
     myHealth: null,
+    nearestFood: null,
     initializeGame: function(data) {
+
+        console.log("Game.initializeGame()")
 
         gameArray = gameBuilder.createGameArray(data)
 
         myHeadPosition = { x: 0, y: 0 } // todo
 
         myHealth = 100
+
+        nearestFood = {x: 1, y: 1 }
     }
 }
