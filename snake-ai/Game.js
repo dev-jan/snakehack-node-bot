ccad575c6a97afe76241e3377ab3bbaa243ee632
marco <marco.palkoska@stud.hslu.ch>
2017-11-11 12:48:14 +0100

    added health to Game

--- snake-ai/Game.js
+++ snake-ai/Game.js
@@ -4,10 +4,13 @@ module.exports = {
 
     gameArray: null,
     myHeadPosition: null,
+    myHealth: null,
     initializeGame: function(data) {
 
         gameArray = gameBuilder.createGameArray(data)
 
         myHeadPosition = { x: 0, y: 0 } // todo
+
+        myHealth = 100
     }
 }
